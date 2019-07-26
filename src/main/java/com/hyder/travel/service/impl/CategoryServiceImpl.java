e5c0c8946581c0f39b2f55fe4fd6edfd93eb4e47
hyder <yanghuan0613@gmail.com>
2019-07-26 12:54:57 +0800

    查询数据加入redis缓存

--- src/main/java/com/hyder/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/hyder/travel/service/impl/CategoryServiceImpl.java
@@ -4,8 +4,12 @@ import com.hyder.travel.dao.CategoryDao;
 import com.hyder.travel.dao.Impl.CategoryDaoImpl;
 import com.hyder.travel.domain.Category;
 import com.hyder.travel.service.CategoryService;
+import com.hyder.travel.util.JedisUtil;
+import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author: 杨欢
@@ -17,6 +21,25 @@ public class CategoryServiceImpl implements CategoryService {
 
 	@Override
 	public List<Category> findAll() {
-		return dao.findAll();
+		Jedis jedis = JedisUtil.getJedis();
+		Set<String> set = jedis.zrange("category", 0, -1);
+		List<Category> all = null;
+
+		if (set == null || set.size() == 0){
+			all = dao.findAll();
+			System.out.println("数据库查询");
+			for (int i = 0; i < all.size(); i++) {
+				jedis.zadd("category", all.get(i).getCid(), all.get(i).getCname());
+			}
+		} else {
+			all = new ArrayList<Category>();
+			System.out.println("redis查询");
+			for (String name : set){
+				Category category = new Category();
+				category.setCname(name);
+				all.add(category);
+			}
+		}
+		return all;
 	}
 }
--- src/main/java/com/hyder/travel/web/servlet/BaseServlet.java
+++ src/main/java/com/hyder/travel/web/servlet/BaseServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -21,11 +20,7 @@ public class BaseServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String uri = req.getRequestURI();
-		System.out.println("url: " + uri);
-
 		String methodName = uri.substring(uri.lastIndexOf('/') + 1);
-		System.out.println("methodName: " + methodName);
-
 		try {
 			Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
 			method.invoke(this, req, resp);
